ba10e071e49218f6f13f35c8dc8f0d2c8824dffb
Said Sezer <docmsaid@gmail.com>
2025-06-22 16:16:09 +0300

    story_generator_v2

---
+++ story_generator_v2.py
@@ -0,0 +1,10 @@
+first_character = input("What is the main character's name? ")
+second_character = input("What is the main character's friend's name? ")
+location = input("Where do they live? ")
+enemy = input("What kind of enemies will appear in the story? ")
+
+print(f"""
+One day, {first_character} and {second_character} were walking through the streets of {location}.
+Suddenly, they saw a group of {enemy} running toward them.
+The {enemy} looked furious, and both {first_character} and {second_character} were terrified.
+""")
